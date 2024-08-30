e57d31bf266a57e11f0d43dee4359750de97fa2d
Jiaying Lu <lujl12@student.unimelb.edu.au>
2024-08-30 23:23:24 +1000

    Creating a Vue Application

---
+++ learnr/src/main.js
@@ -0,0 +1,29 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import Generator from './pages/Generator.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const app = createApp(App);
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        {
+            path: "/",
+            redirect: '/Generator'
+        },
+        {
+            path: "/Generator",
+            name: "Generator",
+            component: Generator,
+          },
+    ]
+  });
+
+app.use(router);
+app.mount("#app");
+
+export default router;
---
+++ learnr/src/pages/Generator.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="width: 409px; height: 386px; background: #156B3A; box-shadow: 560.7999877929688px 560.7999877929688px 560.7999877929688px; border-radius: 9999px; border: 1px black solid; filter: blur(560.80px)"></div>
+  <div style="width: 392px; height: 346px; background: #156B3A; box-shadow: 560.7999877929688px 560.7999877929688px 560.7999877929688px; border-radius: 9999px; border: 1px black solid; filter: blur(560.80px)"></div>
+  <div style="width: 357px; height: 362px; background: #F0A454; box-shadow: 560.7999877929688px 560.7999877929688px 560.7999877929688px; border-radius: 9999px; filter: blur(560.80px)"></div>
+</template>
+  
+<script>
+  
+</script>
+  
+  
