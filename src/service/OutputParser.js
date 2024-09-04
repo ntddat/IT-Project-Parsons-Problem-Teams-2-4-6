d541d42ed5cf5d31ae58ed229bbbbf159c959bc1
josephshim02 <152160739+josephshim02@users.noreply.github.com>
2024-09-04 23:09:18 +1000

    OutputParser now converts python code string into an array of lines of code, filtering out comments and multiple new line characters in a row and handling the edge case of new line characters in strings. Currently some prompts cause the error listed in updatedOutputParserError.txt

--- src/service/OutputParser.js
+++ src/service/OutputParser.js
@@ -1,6 +1,90 @@
 import regex from "../utils/constants/regex.js";
 import messages from "../utils/constants/messages.js";
 
+/**
+ * @function processString
+ * @param {*} string 
+ * Filters out comments
+ * Filters out any \n followed by spaces, tabs or \n
+ * Accounts for print statements containing \n
+ * @returns An array of strings where each string corresponds to a line of python
+ * code to be parsed to the interactive problem
+ */
+function processString(string) {
+  let commentFlag = false;
+  let acceptNewLinesFlag1 = false;
+  let acceptNewLinesFlag2 = false;
+  let newLineFlag = false;
+  //if true then parser will add a new array element
+  let nextLineFlag = false;
+  let codeArray = [];
+  let currentChar = '';
+
+  for (let i = 0; i < string.length; i++) {
+    currentChar = string[i]
+
+    //flags
+
+    //Assuming commens end in a newline character
+    //Also assuming that the ai doesn't write code on the same line as a comment following the comment
+    if (!commentFlag) {
+      if (currentChar == "#") {
+        commentFlag = true;
+      }
+    } else {
+      if (currentChar == "\n") {
+        commentFlag = false;
+        nextLineFlag = true;
+      }
+    }
+
+    //Doesn't handle multiple embedded quotation marks ie: "'""'"
+    if (!acceptNewLinesFlag1) {
+      if (currentChar == "'") {
+        acceptNewLinesFlag1 = true;
+      }
+    } else {
+      if (currentChar == "'") {
+        acceptNewLinesFlag1 = false;
+      }
+    }
+
+    if (!acceptNewLinesFlag2) {
+      if (currentChar == '"') {
+        acceptNewLinesFlag2 = true;
+      }
+    } else {
+      if (currentChar == '"') {
+        acceptNewLinesFlag2 = false;
+      }
+    }
+
+    if (newLineFlag) {
+      if (currentChar != ' ' && currentChar != '\n' && currentChar != '\t') {
+        newLineFlag = false;
+        nextLineFlag = true;
+      }
+    }
+    if (!newLineFlag) {
+      if (currentChar == '\n' && !acceptNewLinesFlag1 && !acceptNewLinesFlag2) {
+        newLineFlag = true;
+      }
+    }
+
+    //adding to array
+    if (!newLineFlag && !commentFlag && (nextLineFlag == true || codeArray.length == 0)) {
+      codeArray.push(currentChar);
+      nextLineFlag = false;
+    } else if (!newLineFlag && !commentFlag) {
+      codeArray[codeArray.length - 1] += currentChar;
+    }
+    
+  }
+
+  return codeArray;
+}
+
+
 /**    
   * @function outputParserJson    
   * Takes the response from Gemini API, presumably in the format
@@ -16,6 +100,7 @@ export function outputParserJson(output) {
     throw messages.INVALID_OUTPUT_FORMAT;
     // throw "Invalid output format";
   }
-  let parsedData = doParse[1];
-  return JSON.parse(parsedData);
+  let parsedData = JSON.parse(doParse[1]);
+  parsedData.Code = processString(parsedData.Code);
+  return parsedData;
 }
