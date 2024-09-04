3acdaa8add92f0aef111dd6a4827d949e80f7fd2
ErnestTayJieJun <161669534+ErnestTayJieJun@users.noreply.github.com>
2024-09-04 17:29:32 +1000

    Merged the code with branch HWP-25 and established connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,1 +1,39 @@
 // DATABASE CONNECTION GOES HERE
+
+const { MongoClient, ListCollectionsCursor } = require('mongodb');
+
+async function main() {
+
+    const uri = "mongodb+srv://parsonsproblemteams246:Password12345@parsonsproblemcluster.ufx52.mongodb.net/?retryWrites=true&w=majority&appName=ParsonsProblemCluster"
+
+    const client = new MongoClient(uri);
+
+    try 
+    {
+        await client.connect();
+        await listDatabases(client);
+        const db = client.db("questions");
+        const coll = db.collection("question_details");
+        const cursor = coll.find();
+        await cursor.forEach(console.log);
+    } 
+
+    catch (e) 
+    {
+        console.error(e);
+    } 
+    
+    finally 
+    {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) 
+{
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
