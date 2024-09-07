522effed498a5433d1c0a363f6a85137a361cab1
josephshim02 <152160739+josephshim02@users.noreply.github.com>
2024-09-07 17:35:26 +1000

    directly uses regex string

--- src/service/OutputParser.js
+++ src/service/OutputParser.js
@@ -1,6 +1,16 @@
 import regex from "../utils/constants/regex.js";
 import messages from "../utils/constants/messages.js";
 
+/**    
+  * @function outputParserJson    
+  * Takes the response from Gemini API, presumably in the format
+  * ```json```, it will return an object of 3 strings, being the code,
+  * code description, and the optional example CSV file in text if applicable
+  * respectively. If the output is not in the correct format, the function 
+  * will throw an "Invalid output format" exception.
+  */
+
+
 /**
  * @function processString
  * @param {*} string 
@@ -84,23 +94,19 @@ function processString(string) {
   return codeArray;
 }
 
-
-/**    
-  * @function outputParserJson    
-  * Takes the response from Gemini API, presumably in the format
-  * ```json```, it will return an object of 3 strings, being the code,
-  * code description, and the optional example CSV file in text if applicable
-  * respectively. If the output is not in the correct format, the function 
-  * will throw an "Invalid output format" exception.
-  */
 export function outputParserJson(output) {
-  // const regexJsonParser = /```json\n([\s\S]*?)```/g;
-  let doParse = regex.REGEX_JSON_PARSER.exec(output);
+  const regexJsonParser = /```json\n([\s\S]*?)```/g;
+  let doParse = regexJsonParser.exec(output);
   if (doParse === null) {
     throw messages.INVALID_OUTPUT_FORMAT;
-    // throw "Invalid output format";
+    
   }
-  let parsedData = JSON.parse(doParse[1]);
+  let parsedData = doParse[1];
+  parsedData = JSON.parse(parsedData); 
+  console.log(parsedData.Code);
+  console.log(parsedData)
   parsedData.Code = processString(parsedData.Code);
+  console.log(parsedData.Code);
   return parsedData;
 }
+
