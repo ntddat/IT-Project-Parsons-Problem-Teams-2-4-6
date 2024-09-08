ce03a5834a4d3fe99e23b0384fc633b7ce157e6e
CHEN XUAN <xuanchen4@student.unimelb.edu.au>
2024-09-08 14:27:17 +1000

    Fix bugs of showing incorrect pagrs

--- learnr/src/main.js
+++ learnr/src/main.js
@@ -3,17 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import Generator from './pages/Generator.vue'
-import AdminLogin from './pages/AdminLogin.vue'
 import AdminProfile from './pages/AdminProfile.vue'
+import AdminLogin from './pages/AdminLogin.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import './assets/icon/iconfont.css'
 
 const app = createApp(App);
 
+
 const router = createRouter({
     history: createWebHistory(),
-    routes: 
-    [
+    routes:[
         {
             path: "/",
             redirect: '/Generator'
@@ -22,19 +22,19 @@ const router = createRouter({
             path: "/Generator",
             name: "Generator",
             component: Generator,
-        },
-        {
-            path: "/AdminLogin",
-            name: "AdminLogin",
-            component: AdminLogin,
-        },
+          },
         {
             path: "/AdminProfile",
             name: "AdminProfile",
             component: AdminProfile,
+          },
+        {
+            path: "/AdminLogin",
+            name: "AdminLogin",
+            component: AdminLogin,
           },  
     ]
-});
+  });
 
 app.use(router);
 app.mount("#app");
