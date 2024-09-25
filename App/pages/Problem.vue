b4d7b39df3028d594c8547dcd80f9ed858a13a62
Jiaying Lu <lujl12@student.unimelb.edu.au>
2024-09-25 22:43:59 +1000

    Change the max height of sortable trash

--- App/pages/Problem.vue
+++ App/pages/Problem.vue
@@ -584,7 +584,7 @@
         /* box-shadow: 0 2px 8px rgba(131, 40, 40, 0.1); */
         flex-shrink: 0;
         overflow-y: auto;
-        max-height: 500px;
+        max-height: 250px;
     }
     
     #regenerate-btn button{
