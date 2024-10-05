9bb8349f7d8867338abd7a0beec3bcabf160df10
SubarudoDuck <minhnghiav@student.unimelb.edu.au>
2024-10-05 16:20:52 +1000

    changed to questions

--- api/database/model/user/userDataModel.js
+++ api/database/model/user/userDataModel.js
@@ -6,12 +6,12 @@ const UserDataSchema = new Schema({
   name: {type: String, required: true, default: "Student"},
   // num attempts and percent correct for each topic
   accuracy: { type: Number, default: 0, required: true },
-  numAttempts: { type: Number, default: 0, required: true },
+  numQuestions: { type: Number, default: 0, required: true },
   numCorrect: { type: Number, default: 0, required: true },
   attemptsSummary: [
     {
       topic: { type: String, required: true},
-      numAttempts: { type: Number, default: 0, required: true },
+      numQuestions: { type: Number, default: 0, required: true },
       numCorrect: { type: Number, default: 0, required: true },
       accuracy: { type: Number, default: 0, required: true },
       totalTime: { type: Number, default: 0, required: true },
